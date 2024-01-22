646404db05d3e3c89c123e36e06ad968c91e5eee
Khardur <p.michalopoulos.1999@gmail.com>
2024-01-22 22:00:02 +0200

    First commit

---
+++ Star__Wars/main.py
@@ -0,0 +1,190 @@
+import argparse
+import json
+from src.star_wars_api import search_star_wars_character, get_homeworld_info
+import datetime
+import os
+from prettytable import PrettyTable
+
+CACHE_FILE = 'cache.json'
+
+def load_cache():
+    try:
+        with open(CACHE_FILE, 'r') as file:
+            loaded_cache = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        loaded_cache = {'characters': {}, 'homeworlds': {}, 'search_history': []}
+    
+    if not isinstance(loaded_cache, dict):
+        loaded_cache = {'characters': {}, 'search_history': []}
+    
+    # Ensure both 'characters' and 'homeworlds' keys are present
+    loaded_cache.setdefault('characters', {})
+    loaded_cache.setdefault('homeworlds', {})
+    loaded_cache.setdefault('search_history', [])
+    
+    return loaded_cache
+
+
+
+def save_cache(cache):
+    with open(CACHE_FILE, 'w') as file:
+        json.dump(cache, file, default=str)  # 'default=str' to handle datetime serialization
+
+
+def display_search_history(cache):
+    search_history = cache.get('search_history', [])
+
+    if not search_history:
+        print("No search history available.")
+    else:
+        # Create a PrettyTable instance with column names
+        table = PrettyTable(["Inputed Name", "Name", "Height", "Mass", "Birth Year", "Timestamp", "Homeworld Name", "Homeworld Population"])  # Update column names here
+
+        # Iterate over the search history and add rows to the table
+        for entry in search_history:
+            inputed_name = entry['name']
+            character_result = entry['result']
+            
+            if character_result is not None:
+                character_info = character_result.get('properties', {})
+                character_name = character_info.get("name", "Name not found")
+                height = character_info.get('height', 'None')
+                mass = character_info.get('mass', 'None')
+                birth_year = character_info.get('birth_year', 'None')
+            else:
+                character_name = "Character not found"
+                height = 'None'
+                mass = 'None'
+                birth_year = 'None'
+            
+            timestamp = entry['timestamp']
+            homeworld_name = entry.get('homeworld_Name', 'None')  # Update this line
+            homeworld_population = entry.get('homeworld_Population', 'None')  # Update this line
+            table.add_row([inputed_name, character_name, height, mass, birth_year, timestamp, homeworld_name, homeworld_population])
+
+        # Set the alignment of columns to left
+        table.align["Inputed Name"] = "l"
+        table.align["Name"] = "l"
+        table.align["Height"] = "l"
+        table.align["Mass"] = "l"
+        table.align["Birth Year"] = "l"
+        table.align["Timestamp"] = "l"
+        table.align["Homeworld Name"] = "l"
+        table.align["Homeworld Population"] = "l"
+
+        # Print the table
+        print(table)
+
+
+
+
+
+
+
+
+def main():
+
+    flag =False
+    flaghome=False
+
+    cache = load_cache()
+
+    #arguments, command line
+    # Create the top-level parser
+    parser = argparse.ArgumentParser(description="Search for a Star Wars character by name.")
+    
+    # Create subparsers for the 'search' and 'clear' subcommands
+    subparsers = parser.add_subparsers(dest='command', help="Subcommands")
+    
+    # Subparser for 'search'
+    search_parser = subparsers.add_parser('search', help="Search for a character")
+    search_parser.add_argument("name", nargs='?', default=None, help="Name of the Star Wars character to search for.")
+    search_parser.add_argument("--world", action="store_true", help="Retrieve homeworld information.")
+
+    # Subparser for 'clear'
+    clear_parser = subparsers.add_parser('clear', help="Clear the search cache")
+
+    # Subparser for 'history'
+    history_parser = subparsers.add_parser('history', help="Display search history")
+
+    args = parser.parse_args()
+
+    #arguments, command line
+
+    # θέλει να ψάξει?
+    if args.command == "search":  
+        #καλει την συναρτηση που ρωταει το api                                     
+        result, flag, last_searched = search_star_wars_character(args.name, cache)
+        save_cache(cache)
+
+        #αν υπαρχει
+        if result:
+            # παιρνει τα αποτελεσματα απο το εμφωλευμενο dictionary                                                      
+            properties = result.get("properties", {})
+            character_name = properties.get("name", "Name not found")  
+            #πριντ τα ντατα
+            print(f"Name: {character_name}")
+            print(f"Height: {properties.get('height', 'Height not found')}")
+            print(f"Mass: {properties.get('mass', 'Mass not found')}")
+            print(f"Birth Year: {properties.get('birth_year', 'Birth Year not found')}")
+
+            #αν θελει και τον κοσμο του χαρακτηρα
+            if args.world:
+
+                #pairnei απο τα ντατα του χαρακτηρα τον κοσμο του
+                homeworld_url = properties.get("homeworld")
+
+                #καλει την συναρτηση που ψαχνει στο api
+                homeworld_data, flaghome, last_searched = get_homeworld_info(homeworld_url, cache)
+                save_cache(cache)
+
+                #an yparxei
+                if homeworld_data:
+                    #ta pairnei apo to εμφωλευμενο dictionary
+                    homeworld_properties = homeworld_data.get("result", {}).get("properties", {})
+                    homeworld_name = homeworld_properties.get("name", "Homeworld Name not found")
+                    homeworld_population = homeworld_properties.get("population", "Homeworld Population not found")
+                    #ta kanei print        
+                    print("\nHomeworld")
+                    print("----------------")
+                    print(f"Name: {homeworld_name}")
+                    print(f"Population: {homeworld_population}")
+                else:
+                    print("Homeworld information not available.")
+            # timestap kai print timestamp
+            if flaghome == False:
+                if flag == False:
+                    timestamp = last_searched
+                else:
+                    timestamp = last_searched
+                print(f"\ncached: {timestamp}")
+            else:
+                timestamp = last_searched
+                print(f"\ncached: {timestamp}")
+
+            #ενημερωνω την λιστα στη cache για το viz
+            if args.world:
+                cache['search_history'].append({'name': args.name, 'result': result, 'timestamp': timestamp, 'homeworld_Name': homeworld_name, 'homeworld_Population': homeworld_population})
+                save_cache(cache)
+            else:
+                cache['search_history'].append({'name': args.name, 'result': result, 'timestamp': timestamp})
+                save_cache(cache)
+
+        #αν δεν υπαρχει
+        else:
+            print("The force is not strong within you")
+            cache['search_history'].append({'name': args.name, 'result': result, 'timestamp': last_searched})
+            save_cache(cache)
+    if args.command == "clear":
+        cache.clear()
+        save_cache(cache)
+        print("Cache cleared")
+
+    #θελει να δει το history    
+    if args.command == "history":
+        display_search_history(cache)
+ 
+
+
+if __name__ == "__main__":
+    main()
---
+++ Star__Wars/src/star_wars_api.py
@@ -0,0 +1,55 @@
+import requests
+from datetime import datetime
+
+import requests
+from datetime import datetime
+
+def search_star_wars_character(character_name, cache):
+    if character_name in cache:
+        flag = True
+        character_data, last_searched = cache[character_name]['data'], cache[character_name]['last_searched']
+        print(f"Retrieved from cache.")
+        return character_data, flag, last_searched
+    else:
+        flag = False
+        base_url = "https://www.swapi.tech/api/people/"
+        query_parameters = {"name": character_name}
+    
+        response = requests.get(base_url, params=query_parameters)
+
+        if response.status_code == 200:
+            data = response.json()
+            results = data.get("result", [])
+            if results:
+                character_data = results[0]
+                homeworld_url = character_data.get("properties", {}).get("homeworld")
+                cache[character_name] = {"data": character_data, "last_searched": datetime.now(), "homeworld_url": homeworld_url}
+                last_searched = datetime.now()
+                return character_data, flag, last_searched
+            else:
+                cache[character_name] = {"data": None, "last_searched": datetime.now(), "homeworld_url": None}
+                last_searched=datetime.now()
+                return None, flag, last_searched
+        else:
+            return None, flag, None
+
+
+
+def get_homeworld_info(homeworld_url, cache):
+    if homeworld_url in cache['homeworlds']:
+        flaghome = True
+        homeworld_info = cache['homeworlds'][homeworld_url]
+        homeworld_data = homeworld_info['data']
+        last_searched = homeworld_info.get('last_searched', 'Not previously cached')
+        print(f"Retrieved also homeworld from cache.")
+        return homeworld_data, flaghome, last_searched 
+    if homeworld_url:
+        flaghome = False
+        response = requests.get(homeworld_url)
+        if response.status_code == 200:
+            homeworld_data = response.json()
+            cache['homeworlds'][homeworld_url] = {"data": homeworld_data,"last_searched": datetime.now()}
+            last_searched=datetime.now()
+            return homeworld_data, flaghome, last_searched
+    return None
+
